1626d6f3caa65d64d965292b812581f0a6b593bb
Salah4220 <habibechesalaheddine1@gmail.com>
2024-04-04 09:52:46 +0200

    Changement de Route en JavaScript

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,25 @@
 <template>
     <div class="login">
       <h1>Login page</h1>
+      <button @click="goToHome">redirect push</button>
+  <button @click="replaceWithHome">Home avec replace</button>
     </div>
   </template>
   
+  <script setup>
+  import { useRouter } from 'vue-router'
+  
+  const router = useRouter();
+  
+  const goToHome = () => {
+    router.push({ path: '/' })
+  }
+  
+  const replaceWithHome = () => {
+    router.replace({ path: '/' })
+  }
+  </script>
+
   <style>
   @media (min-width: 1024px) {
     .login {
@@ -13,4 +29,6 @@
     }
   }
   </style>
+
+
   
